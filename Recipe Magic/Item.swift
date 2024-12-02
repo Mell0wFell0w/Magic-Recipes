b8a7ef45e29cf21df60382855a2c09c2729f32e1
Matt <mccoopton@gmail.com>
2024-12-02 12:05:07 -0700

    Initial Commit

---
+++ Recipe Magic/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Recipe Magic
+//
+//  Created by Matthew Cooper on 12/2/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Recipe MagicTests/Recipe_MagicTests.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_MagicTests.swift
+//  Recipe MagicTests
+//
+//  Created by Matthew Cooper on 12/2/24.
+//
+
+import Testing
+@testable import Recipe_Magic
+
+struct Recipe_MagicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
